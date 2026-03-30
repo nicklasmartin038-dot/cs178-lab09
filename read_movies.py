ed369eabc6ea2b3a66e84e268e30d1b02b2b4ec1
Nicklas Martin <nicklasmartin038@gmail.com>
2026-03-30 12:35:18 -0500

    fixed bug

--- read_movies.py
+++ read_movies.py
@@ -1,4 +1,5 @@
 import boto3
+from boto3.dynamodb.conditions import Attr
 
 REGION = "us-east-1"
 TABLE_NAME = "Movies"
@@ -11,12 +12,12 @@ def print_movie(movie):
     title = movie.get("Title", "Unknown Title")
     year = movie.get("Year", "Unknown Year")
     ratings = movie.get("Ratings", "No ratings")
-    genre = movie.get("Genre", "Unknown Genre")  # ADD
+    genre = movie.get("Genre", "Unknown Genre")
 
     print(f"Title: {title}")
     print(f"Year: {year}")
     print(f"Ratings: {ratings}")
-    print(f"Genre: {genre}")  # ADD
+    print(f"Genre: {genre}")
     print()
 
 def print_all_movies():
@@ -29,5 +30,21 @@ def print_all_movies():
     for movie in items:
         print_movie(movie)
 
+def get_movie_by_title():
+    title = input("Enter movie title: ")
+
+    table = get_table()
+    response = table.scan(
+        FilterExpression=Attr("Title").eq(title)
+    )
+
+    items = response.get("Items", [])
+
+    if items:
+        print("\nMovie found:\n")
+        print_movie(items[0])
+    else:
+        print("\nMovie not found")
+
 if __name__ == "__main__":
-    print_all_movies()
+    get_movie_by_title()
